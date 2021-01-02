5f70a70203f21d33f29ab334e0b8bdf4a9a1eb1f
Hakan ÖZGER <35780405+hknzgr@users.noreply.github.com>
2021-01-02 22:37:24 +0300

    Demo

---
+++ Assets/_scipts/ballController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour {
+
+
+   
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "kup")
+        {
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.tag=="alt")
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+        }
+    }
+
+
+
+}
---
+++ Assets/_scipts/gameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+
+    public GameObject ball;
+    Vector3 mousePos;
+    Vector3 ballPos;
+    Vector3 kuvvet;
+  
+    private void OnMouseUp()
+    {
+            
+            ballPos = ball.transform.position;
+            mousePos = Input.mousePosition;
+            ballPos = Camera.main.WorldToViewportPoint(ballPos);
+            mousePos = Camera.main.ScreenToViewportPoint(mousePos);
+            kuvvet = new Vector3((mousePos.x - ballPos.x) * 1000, 0, (mousePos.y - ballPos.y) * 1000);
+            ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0);
+            ball.GetComponent<Rigidbody>().AddForce(kuvvet);
+           
+    }
+
+   
+}
+
